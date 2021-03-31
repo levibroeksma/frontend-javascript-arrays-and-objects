a00dcde647e1767125a0ce7370c6e0b930b4b848
levibroeksma <levi@broeksma.nl>
2021-03-31 15:10:42 +0200

    first commit, assignments solved

--- arrays.js
+++ arrays.js
@@ -1,18 +1,23 @@
 // ==========================================
 // 1. Declareer een variabele en sla daar een array met vier jaartallen (zoals 2021) in op. Log dit in de terminal.
 // ==========================================
+const years = ['1992', '1993', '1994', '1995'];
 
+console.log(years);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar een array met drie van jouw hobbies (zoals muziek luisteren en koekjes eten) in op. Log dit in de terminal.
 // ==========================================
-
+const hobbies = ['Playing Pokémon', 'Cooking', 'Hiking', 'Spending time with my girlfriend'];
+console.log(hobbies);
 
 // ==========================================
 // 3a. Declareer een variabele met daarin een array met de waardes 3, 4, 5, 7 en 2
 // 3b. Zorg er nu voor dat je de eerste waarde uit die array in de terminal logt             // geeft 3
 // ==========================================
 
+const randomNumbers = [3, 4, 5, 7, 2];
+console.log(randomNumbers[0]);
 
 // ==========================================
 // 4a. Declareer een variabele met daarin een array met de waardes groen, geel, rood, paars, blauw en oranje.
@@ -21,6 +26,8 @@
 // 4c. Zorg ervoor dat je de tweede waarde uit bovenstaande array in de terminal logt       // geeft geel
 // ==========================================
 
+const colours = ['groen', 'geel', 'rood', 'paars', 'blauw', 'oranje'];
+console.log(colours[4] + " " + colours[1]);
 
 // ==========================================
 // 5a. Declareer een variabele met daarin een array met de waardes 21, 22, 23, 25, 25 en log dit in de terminal
@@ -28,10 +35,20 @@
 // 5c. Log de array in de terminal                                                          // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
 
+const randomNumbers1 = [21, 22, 23, 25, 25];
+console.log(randomNumbers1);
 
+randomNumbers1[3] = 24;
+console.log(randomNumbers1)
 // ==========================================
 // 6a. Declareer een variabele met daarin een array met de waardes bladerdeeg, knoflook, spinazie
 // 6b. Overschrijf daarna de waarde bladerdeeg met lasagne bladen (je mag de declaratie op de vorige regel niet aanpassen).
 // 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
+
+const ingredients = ['bladerdeeg', 'knoflook', 'spinazie'];
+
+ingredients[0] = 'lasagne bladen';
+console.log(ingredients);
+console.log(ingredients.length);
